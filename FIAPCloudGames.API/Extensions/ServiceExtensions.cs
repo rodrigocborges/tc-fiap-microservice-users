bb12b44e792bd2bf65a37d419068adb8b745c0f3
Rodrigo Borges <rodrigo.borges@empregare.com.br>
2025-09-28 09:43:12 -0300

    liberação do swagger e add app insights service

--- FIAPCloudGames.API/Extensions/ServiceExtensions.cs
+++ FIAPCloudGames.API/Extensions/ServiceExtensions.cs
@@ -83,15 +83,17 @@ public static class ServiceExtensions
         builder.Services.AddTransient<IUserRepository, UserRepository>();
         builder.Services.AddTransient<IUserService, UserService>();
         #endregion
+
+        builder.Services.AddApplicationInsightsTelemetry(options =>
+        {
+            options.ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"];
+        });
     }
 
     public static void UseServices(this WebApplication app)
     {
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        app.UseSwagger();
+        app.UseSwaggerUI();
 
         app.UseHttpsRedirection();
         app.UseCustomExceptionHandling();
